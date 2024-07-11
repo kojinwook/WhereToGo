164fa364dda16dcacb2a844ee80aa1e9a9579d14
kyj <kimyeji546408@gmail.com>
2024-07-11 10:49:53 +0900

    짜증나

--- src/main/java/com/korea/WhereToGo/common/ResponseCode.java
+++ src/main/java/com/korea/WhereToGo/common/ResponseCode.java
@@ -7,11 +7,8 @@ public interface ResponseCode {
     String DUPLICATE_FESTIVAL = "DF";
     String NOT_EXISTED_FESTIVAL = "NF";
     String DO_NOT_HAVE_PERMISSION = "DHP";
-<<<<<<< HEAD
 
     String NOT_EXISTED_QUESTION = "NQ";
     String NOT_EXISTED_ANSWER = "NA";
-=======
     String NOT_EXISTED_REVIEW = "NR";
->>>>>>> e0cea0363a34047e40832833c879c6d739de2e07
 }
---
+++ src/main/java/com/korea/WhereToGo/controller/MapController.java
@@ -0,0 +1,21 @@
+//package com.korea.WhereToGo.controller;
+//
+//import jakarta.validation.Valid;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequiredArgsConstructor
+//public class MapController {
+//    @Valid("${kakao.api.key}")
+//    private final String apiKey;
+//
+//    @GetMapping("/api/route")
+//    public ResponseEntity<String> getRoute(@RequestParam String origin,
+//                                           @RequestParam String destination){
+//
+//    }
+//}
