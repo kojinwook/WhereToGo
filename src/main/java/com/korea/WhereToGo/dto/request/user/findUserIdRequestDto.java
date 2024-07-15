6b7e278153457b45fa7414da60c12e2001c36273
yusinh <147832320+yusinh@users.noreply.github.com>
2024-07-15 09:50:22 +0900

    오류수정 (#24)
    Co-authored-by: ysinh <내 ìyysh1020@gmail.com>

---
+++ src/main/java/com/korea/WhereToGo/dto/request/user/findUserIdRequestDto.java
@@ -0,0 +1,12 @@
+package com.korea.WhereToGo.dto.request.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class findUserIdRequestDto {
+    private String userId;
+}
